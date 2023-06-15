a4218e4cf0ec9cd5a470463e365ebb8082cde804
Digx7 <37136301+Digx7@users.noreply.github.com>
2023-06-15 12:50:31 -0700

    Saving again

--- Scripts/Follower.cs
+++ Scripts/Follower.cs
@@ -8,24 +8,40 @@ public class Follower : CustomMonoBehaviorWrapper
     [SerializeField] private float followDistance;
     [SerializeField] private float followSpeed;
 
+    private float distanceToTarget{get {return Vector3.Distance(objectToFollow.position, transform.position);}}
+
+    private bool isMoving = false;
+
     private void Update()
     {
+        if(!isMoving)
+        {
+            if(distanceToTarget > followDistance) StartCoroutine(follow());
+        }
+    }
 
-        float distanceToTarget = Vector3.Distance(objectToFollow.position, transform.position);
-
+    private IEnumerator follow()
+    {
+        
         while(distanceToTarget > followDistance)  // is the distance to the target greater than the follow distance?
         {
+            isMoving = true;
+
             Log("Distance to target is " + distanceToTarget);
 
             Vector3 directionTowardsTarget = (objectToFollow.position - transform.position).normalized;  // get direction towards the target
 
             Log("Direction to target is " + directionTowardsTarget);
 
-            Vector3 delta = (directionTowardsTarget * followSpeed * Time.deltaTime);
+            Vector3 delta = (directionTowardsTarget * followSpeed);
 
             Log("Moved by: " + delta);
 
             transform.position += delta;  // move in that direction;
+
+            yield return new WaitForFixedUpdate();
         }
+
+        isMoving = false;
     }
 }
