dc61820c232ee9cebd339bd6fc99f8f307dc7f03
Digx7 <37136301+Digx7@users.noreply.github.com>
2023-06-15 12:38:54 -0700

    About to test snake segments
    Might crash unity

--- Scripts/Follower.cs
+++ Scripts/Follower.cs
@@ -8,13 +8,24 @@ public class Follower : CustomMonoBehaviorWrapper
     [SerializeField] private float followDistance;
     [SerializeField] private float followSpeed;
 
-    private void FixedUpdate()
+    private void Update()
     {
-        while(Vector3.Distance(objectToFollow.position, transform.position) > followDistance)  // is the distance to the target greater than the follow distance?
+
+        float distanceToTarget = Vector3.Distance(objectToFollow.position, transform.position);
+
+        while(distanceToTarget > followDistance)  // is the distance to the target greater than the follow distance?
         {
+            Log("Distance to target is " + distanceToTarget);
+
             Vector3 directionTowardsTarget = (objectToFollow.position - transform.position).normalized;  // get direction towards the target
 
-            transform.position += (directionTowardsTarget * followSpeed);  // move in that direction;
+            Log("Direction to target is " + directionTowardsTarget);
+
+            Vector3 delta = (directionTowardsTarget * followSpeed * Time.deltaTime);
+
+            Log("Moved by: " + delta);
+
+            transform.position += delta;  // move in that direction;
         }
     }
 }
